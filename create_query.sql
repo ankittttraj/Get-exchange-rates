0f6f1efd5abef27a6a34910252c2661507bb2722
Ankit Raj <115062391+ankittttraj@users.noreply.github.com>
2026-01-16 12:03:26 +0530

    Update create_query.sql

--- create_query.sql
+++ create_query.sql
@@ -1,13 +1,14 @@
 -------------------
---Create silver table
+-- Create Silver Table
 -------------------
-CREATE TABLE IF NOT EXISTS `project_name.dataset_name.usd_to_inr_rates` (
+CREATE TABLE IF NOT EXISTS `ankit-data-platform.ds_silver.usd_inr_daily_rates` (
   rate_date        DATE,
   base_currency    STRING,
   target_currency  STRING,
   rate             NUMERIC,
   ingestion_ts     TIMESTAMP,
-  source           STRING
+  source           STRING,
+  pipeline_run_id  STRING
 )
-PARTITION BY rate_date
-CLUSTER BY base_currency, target_currency
+PARTITION BY rate_date;
+
